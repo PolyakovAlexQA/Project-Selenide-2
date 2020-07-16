ccd3dcf4b3aea9e1a95892e482969e656386fa68
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-07-16 12:47:08 +0300

    add one test

--- src/test/java/OrderDeliveryCardTest.java
+++ src/test/java/OrderDeliveryCardTest.java
@@ -1,7 +1,27 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class OrderDeliveryCardTest {
 
+    @Test
+    void OrderDeliveryCardTest(){
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Хабаровск");
+        form.$("[data-test-id=date] input").setValue("21.07.2020");
+        form.$("[data-test-id=name] input").setValue("Поляков Александр");
+        form.$("[data-test-id=phone] input").setValue("+79779778010");
+        form.$("[data-test-id=agreement]").click();
+        form.$$("[role=button]").find(exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
 
+    }
 
 }
